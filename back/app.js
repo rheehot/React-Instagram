d3a834207964108c6dcefa205ddbe105f97e60d8
Haram Bae <hisfedev@gmail.com>
2020-01-20 16:48:14 +0900

    :sparkles: Setup CORS problem
    CORS issue is solved by package

--- back/app.js
+++ back/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./routes/index');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(router);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log('Connected to server!'));
+app.listen(port, () => console.log(`Connected to server on port ${port}!`));
--- back/routes/signup.js
+++ back/routes/signup.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.post('/signup', (req, res) => {
-  console.log(req.body);
+router.post('/', (req, res) => {
+  const query = 'INSERT INTO users (user_name, user_password, user_nickname, user_intro) VALUES (?, ?, ?, ?)';
+  const params = [req.body.id, req.body.password, req.body.nickname, '111'];
+  db.query(query, params, (err, rows) => {
+    if(err) throw new Error(err);
+    console.log(rows);
+  });
 });
 
 module.exports = router;
