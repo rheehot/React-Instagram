645082e92122385c9b3b1e1c8b86a2bd0a125ade
Haram Bae <hisfedev@gmail.com>
2020-01-21 17:56:09 +0900

    :bug: Fix object key error
    Key of object must not be string literal

--- front/src/redux/modules/signup.js
+++ front/src/redux/modules/signup.js
@@ -3,27 +3,33 @@ import { createAction, handleActions } from 'redux-actions';
 export const SIGNUP_REQUEST = 'signup/SIGNUP_REQUEST';
 const SIGNUP_SUCCESS = 'signup/SIGNUP_SUCCESS';
 const SIGNUP_FAILURE = 'signup/SIGNUP_FAILURE';
+const SIGNUP_COMPLETE = 'signup/SIGNUP_COMPLETE';
 
 const initialState = {
   signUpSuccess: false,
   signUpLoading: false,
-  signUpError: '',
+  signUpError: null,
 };
 
 const reducer = handleActions({
-  SIGNUP_REQUEST: (state, action) => ({
+  [SIGNUP_REQUEST]: (state, action) => ({
+    ...state,
     signUpLoading: true,
   }),
 
-  SIGNUP_SUCCESS: (state, action) => ({
+  [SIGNUP_SUCCESS]: (state, action) => ({
+    ...state,
     signUpSuccess: true,
     signUpLoading: false,
   }),
 
-  SIGNUP_FAILURE: (state, action) => ({
+  [SIGNUP_FAILURE]: (state, action) => ({
+    ...state,
     signUpError: action.payload.error,
     signUpLoading: false,
   }),
+
+  [SIGNUP_COMPLETE]: (state, action) => initialState,
 }, initialState);
 
 // id, password, nickname
@@ -31,5 +37,7 @@ export const signUpRequest = createAction(SIGNUP_REQUEST);
 export const signUpSuccess = createAction(SIGNUP_SUCCESS);
 // error message
 export const signUpFailure = createAction(SIGNUP_FAILURE);
+// completion
+export const signUpComplete = createAction(SIGNUP_COMPLETE);
 
 export default reducer;
