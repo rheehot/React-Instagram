baefed0e6d5e8264c13c9640b0994e378c00ad74
Haram Bae <hisfedev@gmail.com>
2020-01-22 11:09:45 +0900

    :sparkles: Apply sequelize
    sequelize

--- back/app.js
+++ back/app.js
@@ -3,6 +3,13 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./routes/index');
+const db = require('./models');
+db.sequelize.sync()
+  .then(() => console.log('DB connection succeeded!'))
+  .catch(err => {
+    console.log('DB connection failed!');
+    process.exit();
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
