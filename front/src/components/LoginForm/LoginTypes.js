5147fa0fbbae1d5762fb4ddb043985ae65ea56b3
Haram Bae <hisfedev@gmail.com>
2020-01-24 23:39:27 +0900

    :wrench: Update types

---
+++ front/src/components/LoginForm/LoginTypes.js
@@ -0,0 +1,19 @@
+export type onSubmitTypes = {
+  id: string,
+  password: string,
+  nickname: string
+};
+
+export type stateTypes = {
+  loginLoading: boolean,
+  loginSuccess: boolean,
+  loginError: boolean
+};
+
+export type fieldTypes = {
+  id: string,
+  name: string,
+  type: string,
+  placeholder: string,
+  icon: string
+};
---
+++ front/src/components/SignUpForm/SignUpTypes.js
@@ -0,0 +1,19 @@
+export type onSubmitTypes = {
+  id: string,
+  password: string,
+  nickname: string
+};
+
+export type stateTypes = {
+  signUpLoading: boolean,
+  signUpSuccess: boolean,
+  signUpError: boolean
+};
+
+export type fieldTypes = {
+  id: string,
+  name: string,
+  type: string,
+  placeholder: string,
+  icon: string
+};
