55f508ce268172cf73a8209fd69e2557fdeb80cb
Haram Bae <hisfedev@gmail.com>
2020-01-24 23:40:07 +0900

    :art: Decouple info

---
+++ front/src/components/LoginForm/LoginField.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: 1,
+    name: 'id',
+    type: 'text',
+    placeholder: '아이디',
+    icon: 'user',
+  },
+  {
+    id: 2,
+    name: 'password',
+    type: 'password',
+    placeholder: '비밀번호',
+    icon: 'lock',
+  },
+];
