253544504b7a497157ec9a77f0c0ffc5bee94d3b
Haram Bae <hisfedev@gmail.com>
2020-01-25 09:53:05 +0900

    :alien: Apply bcrypt and complete passport auth
    Bcrypt and passport

--- back/passport.js
+++ back/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const { users } = require('./models');
+const bcrypt = require('bcryptjs');
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
@@ -17,18 +18,19 @@ module.exports = () => {
     session: true,
     passReqToCallback: false,
   }, (id, password, done) => {
-    console.log(id, password);
     users.findOne({ where: { 'user_name': id }})
       .then((user) => {
         if (!user) {
           return done(null, false, { message: '존재하지 않는 아이디입니다'});
         }
         const dbPassword = user.get('user_password');
-        if (dbPassword === password) {
-          return done(null, user);
-        } else {
-          return done(null, false, { message: '비밀번호가 틀렸습니다' });
-        }
+        bcrypt.compare(password, dbPassword, (err, res) => {
+          if (err) {
+            return done(null, false, { message: '비밀번호가 틀렸습니다' });
+          } else {
+            return done(null, user);
+          }
+        });
       })
       .catch((err) => done(err));
   }));
--- back/routes/auth/signup.js
+++ back/routes/auth/signup.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const { users } = require('../../models');
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+
+const getHashedPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(password, salt);
+    return hashed;
+  } catch (e) {
+    throw new Error('Password hashing failed!');
+  }
+};
 
 router.post('/', asyncHandler(async (req, res) => {
   try {
-    const user = await users.findOrCreate(
-      { 
-        where: { 'user_name': req.body.id }, 
-        defaults: {
-          'user_name': req.body.id,
-          'user_password': req.body.password,
-          'user_nickname': req.body.nickname
-        }
-      }
-    );
-    const isUserExist = !user[1];
-    if (isUserExist) {
+    const user = await users.findOne({ where : { 'user_name': req.body.id } });
+    if (user !== null) {
       throw new Error('중복되는 아이디입니다');
     }
+
+    const hashedPassword = await getHashedPassword(req.body.password);
+    await users.create({
+      user_name: req.body.id,
+      user_password: hashedPassword,
+      user_nickname: req.body.nickname
+    });
     res.status(200).send();
   } catch (e) {
     throw new Error(e);
