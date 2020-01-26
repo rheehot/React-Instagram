3c249bb1b02465f97addec9ffde998fa053f9b65
baeharam <hisfedev@gmail.com>
2020-01-26 21:07:57 +0900

    :sparkles: Include Post and Sidebar
    Signed-off-by: baeharam <hisfedev@gmail.com>

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import { Layout, Card, Icon, Avatar } from 'antd';
+import { Icon } from 'antd';
 import logo from 'images/logo.png';
+import Post from 'components/Post/Post';
 import './Home.scss';
+import SidebarTemplate from 'components/Sidebar/SidebarTemplate/SidebarTemplate';
 
 const Home = () => {
-  const { Header } = Layout;
 
   return(
     <div className="home">
-      <Header className="header">
-        <img className="header__logo" src={logo} alt="로고"/>
-        <Icon className="header__user" type="user" />
-      </Header>
-      <div className="content">
-        <div className="content__post">
-          <div className="content__header">
-            <img className="content__profile" src="https://react-instagram.s3.ap-northeast-2.amazonaws.com/profile.jpg" alt=""/>
-            <div className="content__intro">
-              <p style={{ fontWeight: 'bold' }}>baeharam</p>
-              <p>Coding is life</p>
-            </div>
+      <div className="header">
+        <div className="l-wrapper">
+          <div className="header__container">
+            <img className="header__logo" src={logo} alt="??"/>
+            <Icon className="header__user" type="user" />
           </div>
-          <img className="content__photo" src="https://images.unsplash.com/photo-1579956774928-90599249f90f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=744&q=80" alt=""/>
-          <div className="content__footer">
-            <Icon type="heart" />
+        </div>
+      </div>
+      <div className="content">
+        <div className="l-wrapper">
+          <div className="content__container">
+            <Post />
+            <SidebarTemplate />
           </div>
         </div>
       </div>
